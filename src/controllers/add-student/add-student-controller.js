ba593c9ed23f467ff588c964d80e5f8ed217d4bd
haekalmahendra <haekal762013@gmail.com>
2022-10-12 19:33:20 +0700

    add data validator and change id input method

--- src/controllers/add-student/add-student-controller.js
+++ src/controllers/add-student/add-student-controller.js
@@ -1,16 +1,21 @@
 import bcrypt from "bcrypt";
+import Joi from "joi";
+import { Op } from "sequelize";
 import ShortUniqueId from "short-unique-id";
 import studentModel from "../../db/models";
 import { Respond } from "../../utils/respondFormat";
 import { studentAttributes } from "../../utils/studentAttributes";
 
-const avoidDuplicateName = async (reqBody) => {
+const avoidDuplicateUser = async (reqBody) => {
   const allData = await studentModel.student.count({});
   if (allData > 0) {
     const validateName = await studentModel.student.findOne({
       raw: true,
       attributes: studentAttributes,
-      where: { name: reqBody.name },
+      where: {
+        id: { [Op.like]: reqBody.nrpId },
+        name: { [Op.like]: reqBody.name },
+      },
     });
     return validateName ? false : true;
   }
@@ -18,35 +23,61 @@ const avoidDuplicateName = async (reqBody) => {
 };
 
 const validation = async (requestData) => {
-  const isNotDuplicate = await avoidDuplicateName(requestData);
+  const isNotDuplicate = await avoidDuplicateUser(requestData);
 
   if (!isNotDuplicate) return null;
 
-  const uid = new ShortUniqueId({ length: 10 });
-  var salt = bcrypt.genSaltSync(10);
   const newObj = {
-    id: uid(),
+    id: requestData.nrpId,
     name: requestData.name,
-    password: bcrypt.hashSync(requestData.password, salt),
+    password: bcrypt.hashSync(requestData.password, bcrypt.genSaltSync(10)),
     department_id: requestData.department_id,
     image: "https://i.ibb.co/QNXQM3F/user.png",
   };
 
-  return newObj;
+  const schema = Joi.object({
+    id: Joi.string().min(7).max(7).required().messages({
+      "string.min": "length must be at least 7 characters long",
+      "string.max": "length must be at least 7 characters long",
+    }),
+    name: Joi.string().min(3).max(30).required(),
+    password: Joi.string().required(),
+    department_id: Joi.string().required(),
+    image: Joi.string(),
+  });
+
+  try {
+    const value = await schema.validateAsync(newObj);
+    return value;
+  } catch (err) {
+    return err.details;
+  }
 };
 
 const postCreateStudent = async (req, res, next) => {
+  let validated;
   try {
     const { body } = req;
-    const validated = await validation(body);
+    validated = await validation(body);
     if (validated) {
       await studentModel.student.create(validated);
       res.status(200).json(Respond(200, "Insert User Success", null, ""));
     } else {
-      res.status(400).json(Respond(400, "duplicated data", null, ""));
+      res
+        .status(400)
+        .json(Respond(400, "duplicated data or wrong input", null, ""));
     }
   } catch (err) {
-    res.status(500).json(Respond(500, "", err, ""));
+    res
+      .status(500)
+      .json(
+        Respond(
+          500,
+          validated[0]?.message || err.errors[0].message || null,
+          null,
+          ""
+        )
+      );
   }
 };
 
