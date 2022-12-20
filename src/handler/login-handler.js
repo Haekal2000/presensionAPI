f246de812c1662ecf24f0e4f99306dfbc3af62e2
haekalmahendra <haekal762013@gmail.com>
2022-12-20 14:53:02 +0700

    add api login lecture

--- src/handler/login-handler.js
+++ src/handler/login-handler.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 import studentModel from "../db/models";
 import { studentAttributes } from "../utils/studentAttributes";
 
-export const checkValidationUser = async (param) => {
+const checkValidationStudent = async (param) => {
   const { nrpId, password } = param;
   const { count, rows } = await studentModel.student.findAndCountAll({
     raw: true,
@@ -19,14 +19,43 @@ export const checkValidationUser = async (param) => {
   return false;
 };
 
-export const tokenization = async (param) => {
-  const isValid = await checkValidationUser(param);
+export const checkValidationLecture = async (param) => {
+  const { nik: nikLecture } = param;
+  const { count } = await studentModel.lecturer.findAndCountAll({
+    raw: true,
+    attributes: {
+      exclude: ["id", "departmentId", "roleId"],
+    },
+    where: { nik: nikLecture },
+  });
+
+  if (count === 1) {
+    return true;
+  }
+  return false;
+};
+
+export const tokenization = async (param, isStudent) => {
+  let isValid = false;
+
+  if (isStudent) {
+    isValid = await checkValidationStudent(param);
+  } else {
+    isValid = await checkValidationLecture(param);
+  }
+
   if (isValid) {
-    const { nrpId } = param;
+    const { nrpId, nik } = param;
 
     let arrStr = [];
     const randomize = Math.random().toString(36).substring(2);
-    let arrChar = nrpId.split("");
+    let arrChar = "";
+
+    if (isStudent) {
+      arrChar = nrpId.split("");
+    } else {
+      arrChar = nik.split("");
+    }
 
     arrChar.forEach(() => {
       tempRes = "" + randomize;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import postCreateStudent from "../controllers/add-student/add-student-controller";
 import {
+  postLoginLecture,
   postLoginStudent,
 } from "../controllers/authentication/authentication-controller";
 import { getCourse } from "../controllers/course/course-controller";
@@ -12,11 +13,14 @@ router.post("/student/create", postCreateStudent);
 router.post("/login", async (req, res, next) => {
   await postLoginStudent(req, res, next);
 });
+router.post("/login-lecture", async (req, res, next) => {
+  await postLoginLecture(req, res, next);
+})
 router.get("/course", SecureRoutes, async (req, res, next) => {
   await getCourse(req, res, next);
 });
 router.post("/sendEmail", async (req, res, next) => {
   await sendMail(req, res, next)
-})
+});
 
 export default router;
