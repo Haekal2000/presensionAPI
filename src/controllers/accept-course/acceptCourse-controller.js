7b24ab1d911fe9da41bd26a6f056df1a84e6d1be
haekalmahendra <haekal762013@gmail.com>
2022-12-30 10:44:03 +0700

    validasi course & update database

--- src/controllers/accept-course/acceptCourse-controller.js
+++ src/controllers/accept-course/acceptCourse-controller.js
@@ -2,11 +2,16 @@ import { decrypt } from "../../utils/encryptor";
 import bcrypt from "bcrypt";
 import model from "../../db/models";
 import randomstring from "randomstring";
-import { mode } from "crypto-js";
 
 export const AcceptCourse = (req, res, next) => {
-  const { token, student_id, lecturer_nik, acceptDate, isPresent, schedule_id } =
-    req.body;
+  const {
+    token,
+    student_id,
+    lecturer_nik,
+    acceptDate,
+    isPresent,
+    schedule_id,
+  } = req.body;
 
   const decryptedToken = decrypt(token);
 
@@ -18,42 +23,70 @@ export const AcceptCourse = (req, res, next) => {
     });
   }
 
-  model.schedule
-    .findAndCountAll({
-      raw: true,
+  model.finishedcourse
+    .findOne({
       attributes: {
-        exclude: ["academicperiodId", "courseId", "lecturerId"],
+        exclude: ["finishedcourseId"],
       },
-      where: { course_id: decryptedToken },
+      where: {
+        isPresent: true,
+      },
     })
-    .then(({ count, rows }) => {
-      if (
-        count === 1 &&
-        bcrypt.compareSync(decryptedToken, rows[0]["password"]) === true
-      ) {
-        model.finishedcourse
-          .create({
-            id: randomstring.generate({
-              length: 5,
-              charset: "alphabetic",
-            }),
-            course_id: decryptedToken,
-            ...{ student_id, lecturer_nik, acceptDate, isPresent, schedule_id },
+    .then((item) => {
+      if (item) {
+        res
+          .status(500)
+          .json({ status: 500, message: "Data is Already Exist!" });
+      } else {
+        model.schedule
+          .findAndCountAll({
+            raw: true,
+            attributes: {
+              exclude: ["academicperiodId", "courseId", "lecturerId"],
+            },
+            where: { course_id: decryptedToken },
           })
-          .then(() => {
-            res
-              .status(200)
-              .json({ status: 200, message: "Course Accepted Success!" });
+          .then(({ count, rows }) => {
+            if (
+              count === 1 &&
+              bcrypt.compareSync(decryptedToken, rows[0]["password"]) === true
+            ) {
+              model.finishedcourse
+                .create({
+                  id: randomstring.generate({
+                    length: 5,
+                    charset: "alphabetic",
+                  }),
+                  course_id: decryptedToken,
+                  ...{
+                    student_id,
+                    lecturer_nik,
+                    acceptDate,
+                    isPresent,
+                    schedule_id,
+                  },
+                })
+                .then(() => {
+                  res
+                    .status(200)
+                    .json({ status: 200, message: "Course Accepted Success!" });
+                })
+                .catch((err) => {
+                  res.status(500).json({ status: 500, message: err });
+                });
+            } else {
+              res
+                .status(500)
+                .json({ status: 500, message: "not match course id" });
+            }
           })
           .catch((err) => {
+            console.log("err: ", err);
             res.status(500).json({ status: 500, message: err });
           });
-      } else {
-        res.status(500).json({ status: 500, message: "not match course id" });
       }
     })
     .catch((err) => {
-        console.log('err: ', err);
       res.status(500).json({ status: 500, message: err });
     });
 };
