566a98a1cf1da56032b8c190f713defa6a301abd
haekalmahendra <haekal762013@gmail.com>
2023-01-06 16:41:10 +0700

    add morgan

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import sequelizeConnection from './src/utils/DBconnection';
-import router from './src/routes/routes';
-import * as dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import sequelizeConnection from "./src/utils/DBconnection";
+import router from "./src/routes/routes";
+import * as dotenv from "dotenv";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
@@ -12,22 +13,26 @@ const port = 3000;
 dotenv.config();
 
 app.use(cors());
-
-app.use(cookieParser())
-
+app.use(cookieParser());
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
 app.use(router);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}!`);
 });
 
-sequelizeConnection.authenticate().then(() => {
-  console.log('Connection has been established successfully');
-}).catch((error) => {
-  console.error('Unable to connect to the database: ', error);
-});
+sequelizeConnection
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
